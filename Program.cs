5a50023d5a11e60333b1dfda8020e6b4d3c4bcd9
Breno071 <brenonarde@gmail.com>
2021-12-15 12:13:31 -0300

    Configurações internas

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace TesteTecnico_.NET
     public static async Task Main(string[] args)
     {
       var host = CreateHostBuilder(args).Build();
-      await Seedinicial(host);
+
       var timer = new Timer();
       timer.Interval = TimeSpan.FromMinutes(15).TotalMilliseconds;
       timer.Elapsed += async (sender, e) =>
@@ -48,24 +48,5 @@ namespace TesteTecnico_.NET
             {
               webBuilder.UseStartup<Startup>();
             });
-
-    private static async Task Seedinicial(IHost host)
-    {
-      using (var scope = host.Services.CreateScope())
-      {
-        var services = scope.ServiceProvider;
-        try
-        {
-          await SeedData.SeedData.Inicialize(services);
-          var logger = services.GetRequiredService<ILogger<Program>>();
-          logger.LogInformation("Dados semeados");
-        }
-        catch (Exception ex)
-        {
-          var logger = services.GetRequiredService<ILogger<Program>>();
-          logger.LogError(ex, "Ocorreu um erro ao tentar semear a base de dados");
-        }
-      }
-    }
   }
 }
